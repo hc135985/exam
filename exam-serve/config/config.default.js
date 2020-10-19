c02860b234ecbe1a6bf630bd06e4a2be037a3558
lzz- <704061270@qq.com>
2020-10-19 09:59:02 +0800

    'init'

--- exam-serve/config/config.default.js
+++ exam-serve/config/config.default.js
@@ -15,10 +15,10 @@ exports.security = {
 
 exports.mysql = {
     client: {
-        host: 'localhost',
+        host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: '1234321',
+        password: '12344321',
         database: 'market'
     }
 }
